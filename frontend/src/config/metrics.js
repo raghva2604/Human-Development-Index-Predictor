0540febdce255799abb1d20c623b7913a51ee45d
raghva2604 <raghava3113@gmail.com>
2026-07-01 14:46:47 +0530

    Delete frontend directory

--- frontend/src/config/metrics.js
+++
@@ -1,7 +0,0 @@
-export const MODEL_METADATA = {
-  modelName: 'Gradient Boosting Regressor',
-  accuracy: 98.56,
-  datasetSize: 191,
-  featureCount: 4,
-  datasetName: 'UNDP Human Development Index',
-}
